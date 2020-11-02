faf570d8003dd2eb827f731cc82c152dcd345cbf
GinaNo11 <58694116+GinaNo11@users.noreply.github.com>
2020-11-02 20:08:12 +0300

    Task1

---
+++ retName.js
@@ -0,0 +1,11 @@
+
+var firstName = prompt("enter firstName :");
+var lastName = prompt("enter lastName :"); 
+ 
+function retName(firstName , lastName){
+  return  firstName + " " +lastName ;
+}
+
+var fullName = retName(firstName ,lastName);
+alert(fullName);
+ 
